394bc99b07ffaa45f054ca3863ef229ddc68ca39
stef <stefan.marsiske@gmail.com>
2012-11-30 23:13:15 +0100

    [enh] guard against rate limit on maxmind

--- updatelists.sh
+++ updatelists.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 mkdir -p data
 curl 'http://torstatus.blutmagie.de/ip_list_exit.php/Tor_ip_list_EXIT.csv' -o data/torexits.csv
+curl 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz' | gzip -dc >data/GeoIP.dat.new && mv data/GeoIP.dat.new data/GeoIP.dat
+
+# warning this will overwrite the contributions to this file by pywik
 #curl 'http://www.user-agents.org/allagents.xml' | xmlstarlet sel -t -m '//user-agent' -v 'Type' -o ',' -v 'String' -n >data/agents.csv
-curl 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz' | gzip -dc >data/GeoIP.dat
